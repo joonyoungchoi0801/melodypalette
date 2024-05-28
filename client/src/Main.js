4f369436c84523512c1e34d66f3d5ca5a9e5c59d
roor <02tladbwjd@naver.com>
2024-05-28 22:53:07 +0900

    App.js ->Main.js로 변경

---
+++ client/src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.css';
+// import Login from './Login';
+
+function Main() {
+  return (
+    <div className="Main">
+      <header className="Main-header">
+        <h1>Welcome to the Music Recommendation Platform</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Main;

0f844b27a34950caca2faaba0e7e5e3e48631fbc
roor <02tladbwjd@naver.com>
2024-05-30 22:51:04 +0900

    메인 첫번째 섹션 디자인 수정

--- client/src/Main.js
+++ client/src/Main.js
@@ -2,6 +2,15 @@ import React, { useEffect, useState } from 'react';
 import './Main.css';
 // import Login from './Login';
 
+const albumCovers = [
+  // 예시 앨범 이미지 URL
+  'https://img.etnews.com/news/article/2024/05/23/cms_temp_article_23120344835638.jpg',
+  'http://img.etoday.co.kr/pto_db/2024/05/20240509102337_2022568_1200_1800.jpg',
+  'https://cdn.sports.hankooki.com/news/photo/202404/6861711_1079508_493.jpg',
+  'https://cdn.tenbizt.com/tenbizt/2024/03/11190353/%EC%95%84%EC%9D%BC%EB%A6%BF_%EB%AF%BC%EC%A3%BC-640x427.jpg',
+  'https://image.ytn.co.kr/general/jpg/2024/0125/202401251017584548_d.jpg'
+];
+
 function Main() {
   const [navbarBackground, setNavbarBackground] = useState('transparent');
 
@@ -36,9 +45,16 @@ function Main() {
       </nav>
       <section className="section gradient-background">
         <div className="intro-container">
-          <h1>Welcome to Melodypalette</h1>
-          <p>Your personalized music journey starts here.<br /> Discover new music, track top charts, and create your own playlists.</p>
+          <h3>Melodypalette에서 새로운 음악을 만나보세요</h3>
+          <p className='intro-text'>음악 취향을 분석하여, 당신에게 딱 맞는 곡을 추천해드립니다.</p>
           <button className="recommend-button">음악 추천받기</button>
+          <div className="album-container">
+            {albumCovers.map((cover, index) => (
+              <div key={index} className="album">
+                <img src={cover} alt={`Album ${index + 1}`} />
+              </div>
+            ))}
+          </div>
         </div>
       </section>
       <section className="section black-background">

8691e6193c43028f3679ef1bbc03449717d0f82f
roor <02tladbwjd@naver.com>
2024-05-30 20:58:28 +0900

    메인 섹션 3분할, navbar 생성

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Main.css';
 // import Login from './Login';
 
 function Main() {
+  const [navbarBackground, setNavbarBackground] = useState('transparent');
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > window.innerHeight) {
+      setNavbarBackground('black');
+    } else {
+      setNavbarBackground('transparent');
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div className="Main">
-      <header className="Main-header">
-        <h1>Welcome to the Music Recommendation Platform</h1>
-      </header>
+      <nav className="navbar" style={{ backgroundColor: navbarBackground }}>
+      <div className="navbar-container">
+          <h3 className="logo">Melodypalette</h3>
+          <div className='navbar-box'>
+            <ul>
+              <li><a href="#section1">음악 검색</a></li>
+              <li><a href="#section2">실시간 top 랭킹</a></li>
+              <li><a href="#section3">Playlist</a></li>
+              <li><a href="#section4">프로필</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <section className="section gradient-background">
+        <div className="intro-container">
+          <h1>Welcome to Melodypalette</h1>
+          <p>Your personalized music journey starts here.<br /> Discover new music, track top charts, and create your own playlists.</p>
+          <button className="recommend-button">음악 추천받기</button>
+        </div>
+      </section>
+      <section className="section black-background">
+        <h2>Search Music</h2>
+      </section>
+      <section className="section black-background">
+        <h2>Music TOP 30 Ranking</h2>
+      </section>
     </div>
   );
 }

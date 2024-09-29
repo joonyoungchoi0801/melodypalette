61825ca076cd41553bce6dfbc9471dc7a82b7ec9
roor <02tladbwjd@naver.com>
2024-09-29 17:05:41 +0900

    feat: PlaylistDetail 재생 버튼 기능 추가

--- client/src/components/Playlist/PlaylistDetail.js
+++ client/src/components/Playlist/PlaylistDetail.js
@@ -8,6 +8,8 @@ function PlaylistDetail() {
   const navigate = useNavigate();
   const [playlist, setPlaylist] = useState(null);
 
+  const accessToken = 'BQD3SevM8Ci2IeIEx8LQEIwKCfVDpQhDS3n2sPypp3fxpSKdP-8VoghLlyXh-L2f-aPzLjHYR8U0HAXW3P5vIcqWZ02XudrDZZZ4k9xXalVzn1O-L7IknOO4seWojm2Dow__zBMqJMw0Nrh2wltsXgF7EEQRcjsJ4FT_heHr5aCpyqVJtSxKi8De404vKaBC4R2GTA-6ocLEEwF26ajqzYkSoT3EYH8HIlcapZHz';
+
   // 해당 플레이리스트를 로드하는 함수
   useEffect(() => {
     // 로컬 스토리지에 저장된 플레이리스트를 가져옴
@@ -31,6 +33,13 @@ function PlaylistDetail() {
     return <p>플레이리스트를 찾을 수 없습니다.</p>;
   }
 
+  // 재생 버튼 클릭 핸들러
+  const handlePlay = (track) => {
+    console.log('Playing track:', track);
+    const uri = track.spotifyUrl; // 선택된 곡의 URI
+    navigate(`/player?uri=${encodeURIComponent(uri)}&token=${encodeURIComponent(accessToken)}&name=${encodeURIComponent(track.name)}&artist=${encodeURIComponent(track.artist)}&albumImage=${encodeURIComponent(track.albumImage)}`);
+  };
+
   // 뒤로 가기 버튼 클릭 시 플레이리스트 목록으로 돌아가는 함수
   const goBack = () => {
     navigate('/playlists');
@@ -52,6 +61,12 @@ function PlaylistDetail() {
                 <h3>{track.name}</h3>
                 <p>{track.artist}</p>
               </div>
+              <button 
+                      className='play-button'
+                      onClick={() => handlePlay(track)}
+                    >
+                      ▶️ 재생
+                    </button>
             </div>
           ))
         )}

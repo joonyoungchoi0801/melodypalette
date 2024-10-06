024efd6cd9068b25c5582d9d9f4cba2ba8ecb10c
roor <02tladbwjd@naver.com>
2024-10-06 21:09:13 +0900

    fix: user.js User 모델 require 추가

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
 // 사용자 목록 조회 라우트
 router.get('/', async (req, res) => {
@@ -7,6 +8,7 @@ router.get('/', async (req, res) => {
     const users = await User.find({});
     res.status(200).json(users);
   } catch (error) {
+    console.error('Error fetching users:', error);
     res.status(500).json({ message: '서버 오류', error });
   }
 });

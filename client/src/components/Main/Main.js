0e16fd68edcaca36d4aaaa41ce61d87c0e2f5725
roor <02tladbwjd@naver.com>
2024-10-18 17:42:57 +0900

    design: Main features-section 디자인 수정

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -107,27 +107,45 @@ function Main() {
           <div className="feature-list">
             <div className="feature-item">
               <h3>취향에 맞는 음악 추천</h3>
-              <p>선호하는 아티스트 또는 장르를 선택해, 당신만의 음악을 추천받을 수 있습니다.</p>
+              <img src="/images/feature1.png" alt="feature1"/>
+              <div className='feature-box'>
+                <p>선호하는 아티스트 또는 장르를 선택해,<br /> 당신만의 음악을 추천받을 수 있습니다</p>
+              </div>
             </div>
             <div className="feature-item">
               <h3>나만의 플레이리스트 만들기</h3>
-              <p>추천받은 노래 중 맘에 드는 곡을 모아 나만의 플레이리스트를 만들 수 있습니다.</p>
+              <img src="/images/feature2.png" alt="feature2"/>
+              <div className='feature-box'>
+                <p>추천받은 노래 중 맘에 드는 곡을 모아 <br /> 나만의 플레이리스트를 만들 수 있습니다</p>
+              </div>
             </div>
             <div className="feature-item">
               <h3>음악 재생</h3>
-              <p>추천된 곡들을 바로 들어보면서 음악을 즐길 수 있습니다.</p>
+              <img src="/images/feature3.png" alt="feature3"/>
+              <div className='feature-box'>
+                <p>추천된 곡들을 바로 들어보면서<br/> 음악을 즐길 수 있습니다</p>
+              </div>
             </div>
             <div className="feature-item">
               <h3>좋아요 표시하기</h3>
-              <p>마음에 드는 곡에 좋아요 표시를 해 나만의 컬렉션을 만들어보세요.</p>
+              <img src="/images/feature4.png" alt="feature4"/>
+              <div className='feature-box'>
+                <p>마음에 드는 곡에 좋아요 표시를 해<br/> 나만의 컬렉션을 만들어보세요</p>
+              </div>
             </div>
             <div className="feature-item">
               <h3>실시간 음악 랭킹</h3>
-              <p>현재 인기 있는 음악을 실시간으로 확인하고, 최신 트렌드를 파악할 수 있습니다.</p>
+              <img src="/images/feature5.png" alt="feature5"/>
+              <div className='feature-box'>
+                <p>현재 인기 있는 음악을 실시간으로 확인하고, 최신 트렌드를 파악할 수 있습니다</p>
+              </div>
             </div>
             <div className="feature-item">
               <h3>음악 및 아티스트 검색</h3>
-              <p>원하는 음악이나 아티스트를 검색하여 쉽게 찾아 듣고, 즐길 수 있습니다.</p>
+              <img src="/images/feature6.png" alt="feature6"/>
+              <div className='feature-box'>
+                <p>원하는 음악이나 아티스트를 검색하여 쉽게 찾아 듣고, 즐길 수 있습니다</p>
+              </div>
             </div>
           </div>
         </div>

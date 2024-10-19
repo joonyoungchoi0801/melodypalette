98ee63010a02886c733a1da7ba5453ba1aec51f7
roor <02tladbwjd@naver.com>
2024-10-19 17:53:03 +0900

    design: UserPlaylists 리스트 배치 변경

--- client/src/components/Playlist/UserPlaylists.js
+++ client/src/components/Playlist/UserPlaylists.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
-import './UserPlaylists.css'; 
+import './UserPlaylists.css';
 import Navbar from '../Navbar/Navbar';
 
 function UserPlaylists() {
   const [playlists, setPlaylists] = useState([]);
   const [newPlaylistName, setNewPlaylistName] = useState('');
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const { userProfile } = useAuth();
 
   // 백엔드에서 플레이리스트 가져오기
@@ -25,8 +25,8 @@ function UserPlaylists() {
       .catch((error) => console.error('Error fetching playlists:', error));
   }, [userProfile]);
 
-   // 새로운 플레이리스트 생성 함수
-   const createNewPlaylist = () => {
+  // 새로운 플레이리스트 생성 함수
+  const createNewPlaylist = () => {
     if (newPlaylistName.trim() === '') return;
 
     fetch('http://localhost:5000/api/playlists/create', {
@@ -55,35 +55,41 @@ function UserPlaylists() {
   return (
     <div className="UserPlaylists">
       <Navbar />
-      <h2 className='page-title'>내 플레이리스트</h2>
-      <div className="new-playlist-container">
-        <input 
-          type="text" 
-          value={newPlaylistName} 
-          onChange={(e) => setNewPlaylistName(e.target.value)} 
-          placeholder="새 플레이리스트 이름" 
-          className="new-playlist-input"
-        />
-        <button onClick={createNewPlaylist} className="create-playlist-button">플레이리스트 만들기</button>
-      </div>
-
-      <div className="playlist-container">
-        {playlists.length === 0 ? (
-          <p>아직 플레이리스트가 없습니다.</p>
-        ) : (
-          playlists.map(playlist => (
-            <div 
-              key={playlist._id} 
-              className="playlist-card"
-              onClick={() => goToPlaylistDetail(playlist._id)}
-            >
-              <div className="playlist-info">
-                <h3 className="playlist-name">{playlist.name}</h3>
-                <p className="playlist-track-count">{playlist.tracks.length} 곡</p>
-              </div>
-            </div>
-          ))
-        )}
+      <div className="content-wrapper">
+        <div className="playlist-left-container">
+          <h3 className='page-title'>My playlist</h3>
+          <div>
+            <input
+              type="text"
+              value={newPlaylistName}
+              onChange={(e) => setNewPlaylistName(e.target.value)}
+              placeholder="새 플레이리스트 이름"
+              className="new-playlist-input"
+            />
+            <button onClick={createNewPlaylist} className="create-playlist-button">플레이리스트 만들기</button>
+          </div>
+          <div className="playlist-container">
+            {playlists.length === 0 ? (
+              <p>아직 플레이리스트가 없습니다.</p>
+            ) : (
+              playlists.map(playlist => (
+                <div
+                  key={playlist._id}
+                  className="playlist-card"
+                  onClick={() => goToPlaylistDetail(playlist._id)}
+                >
+                  <div className="playlist-info">
+                    <h3 className="playlist-name">{playlist.name}</h3>
+                    <p className="playlist-track-count">{playlist.tracks.length} 곡</p>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+        <div className='playlist-right-container'>
+          
+        </div>
       </div>
     </div>
   );

c92c705b3328182511fd6fac8b144322686c3b52
roor <02tladbwjd@naver.com>
2024-10-23 20:39:29 +0900

    feat: Main에서 Search 컴포넌트 분리

---
+++ client/src/components/Search/Search.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Search.css';
+
+function Search() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const artists = ["aespa", "BLACKPINK", "QWER", "IU", "DAY6", "NewJeans", "LE SSERAFIM", "KISS OF LIFE", "RIIZE"];
+
+  const handleSearch = () => {
+    console.log('Search button clicked with term:', searchTerm);
+    navigate(`/search?query=${searchTerm}`);
+  };
+
+  return (
+    <section className="section3" id="search-section">
+      <div className='second-container'>
+        <h2 className='second-container-title'>검색</h2>
+        <div className='search-container'>
+          <input
+            type="text"
+            className="search-input"
+            placeholder="검색어를 입력하세요"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button className="search-button" onClick={handleSearch}>검색</button>
+        </div>
+        <p>최근 인기 아티스트</p>
+        <div className="artists-marquee">
+          <div className="artists-marquee-inner">
+            {artists.concat(artists).map((artist, index) => (
+              <div className='artist-name-box' key={index}>
+                <span className="artist-name">{artist}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Search;

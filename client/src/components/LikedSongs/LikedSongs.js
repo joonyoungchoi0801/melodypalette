7a7f4ae5e345ada5fc8c388915da905618b1bce1
roor <02tladbwjd@naver.com>
2024-10-23 21:58:37 +0900

    feat: LikedSongs 재생 버튼 추가

--- client/src/components/LikedSongs/LikedSongs.js
+++ client/src/components/LikedSongs/LikedSongs.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import './LikedSongs.css';
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 import Navbar from '../Navbar/Navbar';
+import Player from '../Player/Player';
+import './LikedSongs.css';
 
 const LikedSongs = () => {
   const { userProfile } = useAuth(); // 사용자 프로필 불러오기
   const userId = userProfile?.id; // 사용자 ID
+  const [accessToken, setAccessToken] = useState(''); // 엑세스 토큰 상태 관리
   const [likedTracks, setLikedTracks] = useState([]); // 좋아요한 트랙들 상태 관리
   const [loading, setLoading] = useState(true); // 로딩 상태 관리
+  const [selectedTrackUri, setSelectedTrackUri] = useState(''); // 재생할 곡 URI 상태 관리
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      setAccessToken(token);
+    } else {
+      console.error('Access token is missing');
+    }
+  }, []);
 
   useEffect(() => {
     if (userId) {
@@ -30,6 +44,18 @@ const LikedSongs = () => {
     return <p>로딩 중...</p>;
   }
 
+  // 재생 버튼 클릭 핸들러
+  const handlePlay = (track) => {
+    const uri = track.spotifyUrl; // 트랙 URI
+    const albumImage = track.album?.images?.[0]?.url || '/images/default_album_image.png'; // 앨범 이미지
+    const artist = track.artists?.[0]?.name || 'Unknown Artist'; // 아티스트 이름
+
+    setSelectedTrackUri(uri);
+
+    // 정보를 전달하며 Player로 이동
+    navigate(`/player?uri=${encodeURIComponent(uri)}&token=${encodeURIComponent(accessToken)}&name=${encodeURIComponent(track.name)}&artist=${encodeURIComponent(artist)}&albumImage=${encodeURIComponent(albumImage)}`);
+  };
+
   return (
     <div className="liked-songs">
       <Navbar />
@@ -43,12 +69,20 @@ const LikedSongs = () => {
                 <h3>{track.name || 'Unknown Title'}</h3>
                 <p>{track.artists?.[0]?.name || 'Unknown Artist'}</p>
               </div>
+              <button
+                className='play-button'
+                onClick={() => handlePlay(track)}
+              >
+                ▶️ 재생
+              </button>
             </li>
           ))}
         </ul>
       ) : (
         <p>좋아요한 곡이 없습니다.</p>
       )}
+      {selectedTrackUri && <Player token={accessToken} trackUri={selectedTrackUri} />}
+
     </div>
   );
 };
